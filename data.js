6d940a63ba3234f99d3ee399a4675416abf46f15
steefanyalvees <stefanyanjos@hotmailcom>
2024-05-20 22:57:42 +0100

    data and variable

---
+++ data.js
@@ -0,0 +1,18 @@
+// tipos premitivos 
+console.log(6.4) //float
+console.log(1) //int
+console.log('pen') // String 
+
+
+// criar uma variavel in JS
+ var name = "Stefany"; // para criar uma variavel 
+ var lastName = "Alves";
+ var todaysYear = 2024
+ var year = 1997
+ var myAgeIs = todaysYear - year
+
+ console.log (lastName)
+ console.log(name)
+ console.log(myAgeIs)
+ console.log ('my age is '+ ' ' + myAgeIs)
+
