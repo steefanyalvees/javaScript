8a76038af2e77debe016eb94c76307ead4443222
steefanyalvees <stefanyanjos@hotmailcom>
2024-05-20 23:20:49 +0100

    changes

---
+++ variabless.js
@@ -0,0 +1,34 @@
+// ways to declar a variable //
+
+// we can use VAR //
+var names = 'Stefany and Jeniffer'
+var age = 27 
+console.log ( names + age) 
+
+// we can name a variable instead of using the var//
+yourName = 'le'
+yourAge = 22;
+yourAddress = 'Seamount'
+console.log(yourAddress + yourName + yourAge)
+
+// we can also use let for varible
+let namess = 'Stefany & Jeniffer'
+let addresses = 'Brazil'
+console.log(namess + addresses)
+
+// definicao de variavel 
+
+// var + identificador do nome da variavel + 
+
+// exemplo conta porcentagem 
+ let produto = 100
+ let desconto = 0.10
+
+  console.log(produto * 1 - desconto)
+ // nesse exemplo a conta n daria certo, 
+ // entao colocamos aspas para priozirar o sinal de menos, assim como na mathematica :
+
+ console.log(produto * (1 - desconto)) // resposta final 
+
+
+
