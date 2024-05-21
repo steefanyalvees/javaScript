1240a82e9d9260c4670d226d329f22001dbc8075
steefanyalvees <stefanyanjos@hotmailcom>
2024-05-21 22:14:28 +0100

    desafios

---
+++ desafioArea.js
@@ -0,0 +1,9 @@
+// PI * raio * raio // calcular a area de uma circuferencia 
+
+let raio = 10;
+const pi = 3.141592;
+let area = pi * raio * raio;
+
+
+console.log( ' the value of the circus is ' + area +' ' + 'metros')
+console.log(Math.PI); // pi disponivel no java sript 
