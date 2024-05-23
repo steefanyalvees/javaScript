be86a7df9b0e18055e2d88069b635f1d43f02566
steefanyalvees <stefanyanjos@hotmailcom>
2024-05-23 06:21:20 +0100

    numbers

---
+++ tiposDados/number.js
@@ -0,0 +1,9 @@
+const myWeight = 65.6
+const myDayOfBday = Number ('7')
+console.log(Number.isInteger(myDayOfBday))
+
+const yearOfBday = Number ('1997')
+console.log(Number.isInteger(yearOfBday))
+
+/*const height = e
+*ouble ('1.65') console.log(double.is) nao tem double nessa linguagem
