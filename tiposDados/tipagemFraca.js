63193d9fca63200b1ec2e38ad717615aac8cb432
steefanyalvees <stefanyanjos@hotmailcom>
2024-05-23 06:08:45 +0100

    tipo de dados

---
+++ tiposDados/tipagemFraca.js
@@ -0,0 +1,6 @@
+let anything = 'hello'
+console.log(anything)
+console.log(typeof anything) 
+/* tipagem fraca n seguinifica que n tem uma varivel sendo declarada, 
+* mas pde ter multiplos valores, diferente do java q nao deixa isso acontecer.
+*/
