fdfc37dec6cc2d1d188c3f77fb8a8ee76062d4c2
steefanyalvees <stefanyanjos@hotmailcom>
2024-05-23 06:01:21 +0100

    folders

--- variaveis_Constante/variaveis3.js
+++ variaveis_Constante/variaveis3.js
@@ -1,3 +1,21 @@
 // como declarar variaveis e constantes
 
 var age = 26 // variavel com identificador, recebe 26
+let lastName = 'Alves' //sempre usar o let, pq e o certo
+/* nao podemos redeclarar let 
+* podemos declarar ariavel 2 veses usando var
+*ex
+ variavel pode mudar o valor
+
+Em JavaScript, uma variável declarada com var pode
+ser atribuída a diferentes valores ao longo do tempo,
+mas ela não armazena múltiplos valores simultaneamente. 
+Em vez disso, cada nova atribuição substitui o valor anterior. 
+
+*/
+
+var city = 'Campinas'
+console.log(city)
+
+var city = 'Sao Paulo'
+console.log (city)
