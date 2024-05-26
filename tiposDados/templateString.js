0622773c019893ad781d296785dd68be840309ae
steefanyalvees <stefanyanjos@hotmailcom>
2024-05-26 13:29:31 +0100

    how to use the string templates

---
+++ tiposDados/templateString.js
@@ -0,0 +1,45 @@
+const nome = 'Rebeca'
+const age = '12'
+const address = 'Brazil'
+//concatenacao
+
+console.log( nome + age + address)
+
+/* nao podemos ter quebra de linha dentro de uma string
+* mas e possivel fazer isso no template String 
+exemplo bellow */ 
+
+// nesse exemplo, ele vai mostrar todas as linhas puladas, espacos
+const template = ` 
+ Hello 
+ ${nome} 
+ e
+  linda
+
+`
+console.log(template)
+
+const birthday = 'Wednesday'
+
+const name = `
+her bday is on ${birthday}, 
+we are happpy for her
+we love her
+ happy 
+ ${birthday}
+`
+console.log(name)
+
+const count = ('Lets count together')
+const numb = ` ${count }
+1
+2
+3
+4
+5
+6
+7
+8
+9`
+
+console.log (numb)
