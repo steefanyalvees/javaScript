65400ede360538b81b8b21cd989e50283a1ecda6
steefanyalvees <stefanyanjos@hotmailcom>
2024-05-26 15:19:24 +0100

    objects

---
+++ tiposDados/objects.js
@@ -0,0 +1,26 @@
+
+/* {} e um object, object e uma colecao de chaves e valores
+* object pode ter outros 
+* objectos dentro dele. Colecao de chave e valor.
+
+*/
+// primeira froma de fazer//
+
+const groceries = {}
+groceries.fruta = ' Apple', 'babanas', 'strawberries' //somente mostra o primeiro valor
+groceries.dairy = ['Iogurt', 'milk', 'cheesse'] // para mostrar tudo, colocamos dentro de um array
+groceries.pastries = ['croassant', 'cinamon roll']
+groceries.offLicenses = ['beer', 'wine','coke', 'juice']
+console.log( groceries)
+
+// forma dois //
+
+const groceries_2 = {
+ fruta:['banana','apple'],
+ dairy:['milk','cheesse'],
+ drink:['wine']
+}
+console.log ( groceries_2)
+
+ 
+
