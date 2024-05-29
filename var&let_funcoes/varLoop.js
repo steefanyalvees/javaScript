217ea56f507f2c8ae525aab85ccacb058071c13c
steefanyalvees <stefanyanjos@hotmailcom>
2024-05-29 05:45:07 +0100

    for loop var

---
+++ var&let_funcoes/varLoop.js
@@ -0,0 +1,15 @@
+// for, if,while,
+// se i for menor que 10 == true 
+for (var i = 0; i<10; i++)
+  {
+    console.log(i)
+  }
+  console.log('i', i ) // i esta visivel aqui pois var n se importa com 
+  // os blocos, e resultado gerado e 10 pq foi o 10 que fez o i sai do laco
+
+for (var year = 2019; year >2022; i++){
+
+console.log('I am bigger than 2024')
+}
+console.log('I am smaller than',year) //sera executado quando sair do loop
+
