7e815bdac78f3ca3288ff88d02b69c0152e59c1c
steefanyalvees <stefanyanjos@hotmailcom>
2024-05-30 22:33:11 +0100

    object

---
+++ funcao/funcaovsObject.js
@@ -0,0 +1,12 @@
+// funcao very importante
+console.log(typeof Object) // isso e uma funcao
+console.log(typeof new Object)// instanciando uma funcao e criando um objeto a partir disso
+console.log(typeof Object())// msm coisa
+const Client = function(){}
+console.log(typeof Client)
+
+//class sao funcoes
+class Produto{}
+console.log(typeof Produto)
+
+// criar object in javaScrip sao feitos com funcoes. 
---
+++ funcao/object.js
@@ -0,0 +1,13 @@
+//obejct colecao de chave e valor
+const year = '2022' // contexto lexico 1 {onde a variavel esta definida, fisico: broswer,file}
+
+function exec(){ // funcao 
+    const year = '2024' // const dentro da funcao = context lexico 2
+    return year
+
+}
+
+//objetos sao grupos aninhados de pares nome/valor
+const client = {
+  nome:
+}
