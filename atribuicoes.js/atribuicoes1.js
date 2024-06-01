7c0814338cc17f26dd5c33dfabad65010df66569
steefanyalvees <stefanyanjos@hotmailcom>
2024-06-01 20:42:38 +0100

    atribuicoes

--- atribuicoes.js/atribuicoes1.js
+++ atribuicoes.js/atribuicoes1.js
@@ -4,3 +4,5 @@ let b = 3 // atribuindo o valor
 console.log(b += a) // b + a // b recebe o valor de a. 
 console.log(b)
 console.log(a)
+console.log(b/=2) // o valor de b muda de 3 para (7+3) = 10
+console.log(b*=2) // total 10 multiplicado por 2 = 10
