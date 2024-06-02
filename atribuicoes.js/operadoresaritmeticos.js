6b27ea9d609e0905608ac31de5261155ca399ae2
steefanyalvees <stefanyanjos@hotmailcom>
2024-06-02 18:16:55 +0100

    aritmetrics

---
+++ atribuicoes.js/operadoresaritmeticos.js
@@ -0,0 +1,11 @@
+//usando o destructure
+const [a,b,c,d] = [3,5,1,15]
+
+// binario= 2 operantes ++d, a-b
+// unario = -4 
+
+const add = a+b+c+d
+const sub = a-b-c-d
+const divi = a*c
+const modulo = a % c // resto da divisao
+console.log(add, sub,divi,modulo)
