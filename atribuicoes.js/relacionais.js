8b4ca58c56018d87c3baba58e30aab7dd0c891c9
steefanyalvees <stefanyanjos@hotmailcom>
2024-06-02 18:35:16 +0100

    relacionais

---
+++ atribuicoes.js/relacionais.js
@@ -0,0 +1,12 @@
+console.log('01', '1'==1) // 1 e igual a 1 = True
+console.log('02', '2'===2)// 2 string e igual a 2 int = False ( estritamente igual)
+console.log('03','3' != 3)// 3 e diferente de 3 = False
+console.log('03', '03' !==3)// 3 string e diferente de 3 integer = True
+
+console.log( 3>2)// 3 maior que 2 - True
+console.log(3<1)// 3 menor que 1 - false
+console.log(3>=3)// true
+
+const d1= new Date(0)
+const d2=new Date(0)
+console.log(d1==d2) //false, diferente memory
