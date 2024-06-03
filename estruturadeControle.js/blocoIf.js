c47502bb5b8827a88d7d47fa258e60508e6de643
steefanyalvees <stefanyanjos@hotmailcom>
2024-06-03 12:00:13 +0100

    metodo e funcao

---
+++ estruturadeControle.js/blocoIf.js
@@ -0,0 +1,12 @@
+// estrutura de controle IF
+//console.log(' hello user, please show me your id')
+
+const age = 4
+
+if (age >22)
+{
+
+  console.log('seu numero e igual ou maior que ' + age)
+}else {
+  console.log('is not bigger than 22, try it again')
+}
---
+++ estruturadeControle.js/metodovsFuncao.js
@@ -0,0 +1,30 @@
+//*Função
+//  Uma função é um bloco de código projetado para realizar uma tarefa
+//  específica.
+//  É uma entidade independente que pode ser definida e chamada 
+//  independentemente de qualquer objeto.
+//  Funções podem receber argumentos, realizar operações e retornar 
+//  valores.
+
+function saudacao(nome) {
+  return `Olá, ${nome}!`;
+}
+
+// Chamando a função
+console.log(saudacao('Alice'));
+
+// Método
+// Um método é semelhante a uma função em termos de funcionalidade,
+//  mas está associado a um objeto. 
+//  Métodos são funções que pertencem a um objeto e 
+//  são usados para operar sobre os dados contidos dentro do objeto.
+
+const pessoa = {
+  nome: 'Bob',
+  saudar: function() {
+      return `Olá, ${this.nome}!`;
+  }
+};
+
+// Chamando o método
+console.log(pessoa.saudar());
