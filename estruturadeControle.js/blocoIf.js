731c2245591a5517df1da5e3082260b622333fb7
steefanyalvees <stefanyanjos@hotmailcom>
2024-06-03 12:54:22 +0100

    if and else

--- estruturadeControle.js/blocoIf.js
+++ estruturadeControle.js/blocoIf.js
@@ -1,12 +1,11 @@
 // estrutura de controle IF
 //console.log(' hello user, please show me your id')
+function partyAge(idade) {
+   if (idade > 18)
+    {
+       console.log('welcome to the club, you are ' + idade)
+    } else (console.log('GET OUT, YOU ARE ONLY ' + idade))
+  }
+  partyAge (22)
+partyAge(12)
 
-const age = 4
-
-if (age >22)
-{
-
-  console.log('seu numero e igual ou maior que ' + age)
-}else {
-  console.log('is not bigger than 22, try it again')
-}
