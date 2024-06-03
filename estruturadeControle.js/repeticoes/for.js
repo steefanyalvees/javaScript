578e44fe7342e938e29b6150b955f1af16d7b385
steefanyalvees <stefanyanjos@hotmailcom>
2024-06-03 19:46:23 +0100

    for

---
+++ estruturadeControle.js/repeticoes/for.js
@@ -0,0 +1,20 @@
+let contador = 7
+while (contador <= 10){ //menor igual a 10
+    console.log(`contador recebe: ${contador}`)
+    contador++ // toda vez que o o loop acontecer,vai add 1, ate chegar no 10
+               // e a expressao se tornar falsa
+
+}
+// mesma expressao usando for 
+
+for (let i =1; i<10; i++)
+  {console.log(`i recebe = ${i}`)} // msm coisa do exemplo anterior 
+
+// exemplo com array 
+
+const notas= [6.6, 7.4,7.8,7.9]
+for(let 
+  i=0; i<notas.length; i++){
+  console.log(`notas = ${notas[i]}`)
+}  // lenght - tamanho do array 
+// comeca no 0, ja que o array comeca no 0
